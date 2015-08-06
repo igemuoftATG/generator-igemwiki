114a60b9ab7e93d93c3159496deda50a6fcb7741
Julian Mazzitelli <mazzitelli.julian@gmail.com>
2015-08-06 19:36:04 -0400

    loads in templateData for hbs from file

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,28 @@
 var gulp = require('gulp');
 var handlebars = require('gulp-compile-handlebars');
 var rename = require('gulp-rename');
+var fs = require('fs');
 
-gulp.task('default', function () {
-    var templateData = {
-        firstName: 'iGEM Toronto'
-    },
-    options = {
-        ignorePartials: true, //ignores the unknown footer2 partial in the handlebars template, defaults to false
-        partials : {
-            footer : '<footer>the end</footer>'
-        },
-        batch : ['./src/partials'],
-        helpers : {
-            capitals : function(str){
-                return str.toUpperCase();
-            }
+var paths = {
+    partials: './src/partials'
+}
+
+var hbsOptions = {
+    batch : [paths.partials],
+    helpers : {
+        capitals : function(str){
+            return str.toUpperCase();
         }
     }
+}
 
+templateDataDev = JSON.parse(fs.readFileSync('./src/templates/template-dev.json'))
+
+gulp.task('handlebars', function() {
     return gulp.src('src/hello.hbs')
-        .pipe(handlebars(templateData, options))
+        .pipe(handlebars(templateDataDev, hbsOptions))
         .pipe(rename('hello.html'))
         .pipe(gulp.dest('build-dev'));
-});
+})
+
+gulp.task('default', ['handlebars']);
