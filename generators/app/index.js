aaa482baae1d01f476fdb5a9c48068cbec0683ea
Julian Mazzitelli <mazzitelli.julian@gmail.com>
2015-09-04 19:10:19 -0400

    asks for repo, --skip-repo option works

--- generators/app/index.js
+++ generators/app/index.js
@@ -13,6 +13,9 @@
 // module.exports = generators.NamedBase.extend();
 
 var generators = require('yeoman-generator');
+var colors = require('colors');
+var requestSync = require('sync-request');
+var cheerio = require('cheerio');
 
 module.exports = generators.Base.extend({
 	// The name `constructor` is important here
@@ -20,13 +23,102 @@ module.exports = generators.Base.extend({
 		// Calling the super constructor is important so our generator is correctly set up
 		generators.Base.apply(this, arguments);
 
-		// Next, add your custom code
-		this.option('coffee'); // This method adds support for a `--coffee` flag
+		// --skip-install flag
+		this.option('skip-install');
+		this.option('skip-repo');
 	},
-	method1: function() {
-		console.log('method 1 just ran');
+	initializing: function() {
+		// this.log('destinationRoot', this.destinationRoot());
+		// this.log('sourceRoot', this.sourceRoot());
 	},
-	method2: function() {
-		console.log('method 2 just ran');
+	prompting: function() {
+		var done = this.async();
+
+		var questions = [{
+			type: 'input',
+			name: 'year',
+			message: 'What year is it?',
+			default: (new Date()).getFullYear()
+		}, {
+			type: 'input',
+			name: 'teamName',
+			message: 'What is your team name ' + 'exactly'.red + ' as it appears on the wiki? (Team:' + 'teamName'.magenta + ')',
+			validate: function(input) {
+				if (input === '') {
+					return false;
+				} else {
+					return true;
+				}
+			}
+		}];
+
+		if (!this.options['skip-repo']) {
+			questions.push({
+					type: 'input',
+					name: 'repo',
+					message: 'What is the GitHub repository for this project? (Provided as ' + 'username/repo'.magenta + ')',
+					validate: function(input) {
+						var good = false
+						for (var i = 0; i < input.length; i++) {
+							if (input[i] === '/' && good === false) {
+								good = true;
+							} else if (input[i] === '/' && good === true) {
+								good = false;
+								return;
+							}
+						}
+
+						if (!good)
+							return 'Follow ' + 'username'.magenta + '/' + 'repo'.magenta + ' format.'
+
+						base_url = 'https://github.com/'
+
+						var httpResponse = requestSync('GET', base_url + input);
+
+						if (httpResponse.statusCode === 200) {
+							return true
+						} else if (httpResponse.statusCode === 404) {
+							return 'Nice try, the page ' + base_url.blue + input.blue + ' returned a ' + '404'.red
+						} else {
+							return false
+						}
+					}
+				})
+			}
+
+			this.prompt(questions, function(answers) {
+				this.answers = {
+						year: answers.year,
+						teamName: answers.teamName,
+						repo: answers.repo
+					}
+					// this.log(answers.year);
+					// this.log(answers.teamName)
+				done();
+			}.bind(this));
+	},
+	configuring: function() {
+		this.log('configuring');
+	},
+	default: function() {
+		this.log('default');
+	},
+	writing: function() {
+		// this.log('writing');
+		this.fs.copyTpl(
+			this.templatePath('template.json'),
+			this.destinationPath('src/template.json'),
+			this.answers
+		)
+	},
+	install: function() {
+		if (this.options['skip-install']) {
+			this.log('Skipping ' + 'npm install'.magenta + ' and ' + 'bower install'.magenta + '. Run these yourself.')
+		} else {
+			this.installDependencies()
+		}
+	},
+	end: function() {
+		this.log('end');
 	}
 });
